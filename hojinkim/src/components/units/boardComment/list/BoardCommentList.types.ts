48c359f7b154c06e234f3f42c8913df41c146049
tinypanda57 <tinypanda57@gmail.com>
2024-12-19 18:53:16 -0600

    12-19-20024

--- hojinkim/src/components/units/boardComment/list/BoardCommentList.types.ts
+++ hojinkim/src/components/units/boardComment/list/BoardCommentList.types.ts
@@ -1,7 +1,10 @@
-import { MouseEvent } from "react";
+import { ChangeEvent, MouseEvent } from "react";
 import { IQuery } from "../../../../commons/types/generated/types";
 
 export interface BoardCommentListUiProps {
   data?: Pick<IQuery, "fetchBoardComments">;
-  onClickDelete: (event: MouseEvent<HTMLImageElement>) => void;
+  onClickDelete: (event: MouseEvent<HTMLButtonElement>) => void;
+  isOpenDeleteModal: boolean;
+  onClickOpenDeleteModal: (event: MouseEvent<HTMLImageElement>) => void;
+  onChangeDeletePassword: (event: ChangeEvent<HTMLInputElement>) => void;
 }
